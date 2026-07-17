9eec3ead35dd8683b9c9bfd1d252ecb651da70cf
samuels-ece <samuelabi2006@gmail.com>
2026-07-17 06:38:22 +0530

    Implement theme toggle button functionality
    Added theme toggle functionality with localStorage support.

--- admin.js
+++ admin.js
@@ -307,3 +307,21 @@ document.getElementById("exportPdfBtn").addEventListener("click", function () {
   pdf.save("CampusFix_Report.pdf");
 
 });
+const themeBtn = document.getElementById("themeBtn");
+
+if (localStorage.getItem("theme") === "dark") {
+  document.body.classList.add("dark");
+  themeBtn.textContent = "☀️";
+}
+
+themeBtn.addEventListener("click", function () {
+  document.body.classList.toggle("dark");
+
+  if (document.body.classList.contains("dark")) {
+    localStorage.setItem("theme", "dark");
+    themeBtn.textContent = "☀️";
+  } else {
+    localStorage.setItem("theme", "light");
+    themeBtn.textContent = "🌙";
+  }
+});
